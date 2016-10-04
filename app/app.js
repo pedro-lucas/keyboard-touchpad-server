e28d1447c44ef315e5f3002c11ef5508fe48241a
Pedro Lucas <lucasdasilvajunior@yahoo.com.br>
2016-10-04 02:20:49 -0300

    Added manager to bluetooth peripheral

--- app/app.js
+++ app/app.js
@@ -3,27 +3,76 @@
 const electron = require('electron');
 const {app, Menu, Tray, BrowserWindow} = electron;
 const path = require('path');
+const KTServer = require('./services/server');
 
 let winMonitor = null;
 let tray = null;
 
+if(process.platform == 'darwin') {
+  app.dock.hide();
+}
+
 app.on('ready', () => {
 
+  const workArea = electron.screen.getPrimaryDisplay().workAreaSize;
+
   tray = new Tray(path.resolve(__dirname, 'resources', 'keyboard.png'));
-  winMonitor = new BrowserWindow({width: 800, height: 600, minWidth: 400, minHeight: 400, show: false});
+
+  winMonitor = new BrowserWindow({
+    width: 400,
+    height: 400,
+    frame: false,
+    show: false
+  });
 
   winMonitor.on('closed', () => {
-    winMonitor = null
+    winMonitor = null;
+  });
+
+  winMonitor.on('blur', () => {
+    winMonitor.hide();
   });
 
   winMonitor.loadURL(`file://${__dirname}/static/index.html`);
 
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Open Monitor', click() {winMonitor.isVisible() ? winMonitor.hide() : winMonitor.show();}},
-    {label: 'Quit', click() {app.quit();}}
+    {
+      label: 'Open Monitor',
+
+      click() {
+
+        const bounds = tray.getBounds();
+        let x = bounds.x;
+        let y = bounds.y;
+
+        if(x + winMonitor.width > workArea.width) {
+          x = workArea.width - winMonitor.width;
+        }
+
+        if(y + winMonitor.height > workArea.height) {
+          y = workArea.height - winMonitor.height;
+        }
+
+        winMonitor.setPosition(x, y);
+        winMonitor.isVisible() ? winMonitor.hide() : winMonitor.show();
+
+      }
+
+    },
+    {
+      label: 'Quit',
+
+      click() {
+        app.quit();
+      }
+
+    }
+
   ]);
 
-  tray.setToolTip('This is my application.');
+  tray.setToolTip('Keyboard Touchpad Server');
   tray.setContextMenu(contextMenu);
 
 });
+
+KTServer.start();
--- app/services/server.js
+++ app/services/server.js
@@ -2,50 +2,96 @@
 
 const bleno = require('bleno');
 const KeyboardPadService = require('./keyboard-touchpad-service');
+const kServerUUID = 'b0d0';
 
-bleno.on('stateChange', function(state) {
+const manager = {
+  state: 'poweredOn',
+  advertising: false,
+  start() {
+    this.state = 'poweredOn';
+    this.startBleno();
+  },
+  stop() {
+    this.state = 'poweredOff';
+    this.stopBleno();
+  },
+  restart() {
+    this.stop();
+    this.start();
+  },
+  startBleno() {
+    if(bleno.state === 'poweredOn' && !this.advertising) {
+      this.advertising = true;
+      bleno.startAdvertising('Keyboard Pad', [kServerUUID]);
+    }
+  },
+  stopBleno() {
+    if(!this.advertising) {
+      bleno.stopAdvertising();
+      this.advertising = false;
+    }
+  },
+  isBluetoothOn() {
+    return bleno.state === 'poweredOn';
+  },
+  isServerOn() {
+    return this.state === 'poweredOn';
+  },
+  isAdvertising() {
+    return advertising;
+  },
+  get bluetoothState() {
+    return bleno.state;
+  }
+};
+
+module.exports = manager;
+
+bleno.on('stateChange', state => {
   console.log('on -> stateChange: ' + state + ', address = ' + bleno.address);
-  if (state === 'poweredOn') {
-    bleno.startAdvertising('Keyboard Pad', ['b0d0']);
-  } else {
-    bleno.stopAdvertising();
+  if (manager.state === 'poweredOn' && state === 'poweredOn') {
+    manager.startBleno();
+  }else{
+    manager.stopBleno();
   }
 });
 
 /* Linux events only */
 
-bleno.on('accept', function(clientAddress) {
+bleno.on('accept', clientAddress => {
   console.log('on -> accept, client: ' + clientAddress);
   bleno.updateRssi();
 });
 
-bleno.on('disconnect', function(clientAddress) {
+bleno.on('disconnect', clientAddress => {
   console.log('on -> disconnect, client: ' + clientAddress);
 });
 
-bleno.on('rssiUpdate', function(rssi) {
+bleno.on('rssiUpdate', rssi => {
   console.log('on -> rssiUpdate: ' + rssi);
 });
 
 /* End Linux only events */
 
-bleno.on('mtuChange', function(mtu) {
+bleno.on('mtuChange', mtu => {
   console.log('on -> mtuChange: ' + mtu);
 });
 
-bleno.on('advertisingStart', function(error) {
-  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
-  if (!error) {
+bleno.on('advertisingStart', err => {
+  console.log('on -> advertisingStart: ' + (err ? 'error ' + err : 'success'));
+  if (!err) {
+    manager.advertising = true;
     bleno.setServices([
       new KeyboardPadService()
     ]);
   }
 });
 
-bleno.on('advertisingStop', function() {
+bleno.on('advertisingStop', () => {
+  manager.advertising = false;
   console.log('on -> advertisingStop');
 });
 
-bleno.on('servicesSet', function(error) {
-  console.log('on -> servicesSet: ' + (error ? 'error ' + error : 'success'));
+bleno.on('servicesSet', err => {
+  console.log('on -> servicesSet: ' + (err ? 'error ' + err : 'success'));
 });
