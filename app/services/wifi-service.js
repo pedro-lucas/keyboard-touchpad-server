ff1fce93ec161baf4bc0aacd723c7efa12e50024
Pedro Lucas <lucasdasilvajunior@yahoo.com.br>
2017-10-01 22:30:39 -0300

    Adicionado o socket io ao projeto

--- app/services/wifi-service.js
+++ app/services/wifi-service.js
@@ -1,9 +1,21 @@
 const Service = require('./service');
+const mdns = require('mdns');
+const io = require('socket.io')(9453);
 
 class WifiService extends Service {
+
   constructor() {
     super();
+    this.init();
   }
+
+  init() {
+    //TODO: O servidor fica ativo.
+    //TODO: O Device solicita conexão.
+    //TODO: O servidor pegunta se pode conectar, caso ainda não esteja na lista de autorizados
+    //TODO: Após conectar pode iniciar o envio de dados. Vamos utilizar o socket IO no projeto
+  }
+
 }
 
 module.exports = new WifiService();
